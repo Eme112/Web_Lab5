b119d7e95b9e66de68020765f77ee769d7bb25a3
Eme112 <eme_pedraza@hotmail.com>
2022-07-16 21:55:57 -0500

    finished version 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
 // 1) Reemplaza estas variables por useState, puedes utilizar los valores iniciales que ya traen las variables
-  let temperatureColor = 'cold'
-  let temperatureValue = '10'
+  const [temperatureColor, setTemperatureColor] = useState('cold')
+  const [temperatureValue, setTemperatureValue] = useState(10)
   
 // 2) Agregar las funciones para manejar los eventos de Click a los botones para que 
 //     cuando se presionen se incremente o decremente el valor de la temperatura. 
-
+  
 // 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
 //    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
+  function updateColor() {
+    if(temperatureValue >= 20) {
+      setTemperatureColor('hot')
+    } else {
+      setTemperatureColor('cold')
+    }
+  }
 
 // Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
 // De igual manera al llegar los valores a 0 no se podrá decrementas más.
@@ -23,8 +30,20 @@ function App() {
         </div>
       </div>
       <div className="button-container">
-        <button >+</button>
-        <button >-</button>
+        <button onClick={()=>setTemperatureValue((prev)=>{
+          updateColor()
+          if(prev < 30)
+            return prev+1
+          else
+            return prev
+        })}>+</button>
+        <button onClick={()=>setTemperatureValue((prev)=>{
+          updateColor()
+          if(prev > 0)
+          return prev-1
+        else
+          return prev
+        })}>-</button>
       </div>
     </div>
   );
